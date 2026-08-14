e5c00f797f33ebfb42d768b7f63350d1b5de84fc
joseferreiradev-commits <245466110+joseferreiradev-commits@users.noreply.github.com>
2026-08-14 01:07:53 -0400

    Activate PWA updates immediately

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const CACHE = 'scrap-catcher-v4';
+const CACHE = 'scrap-catcher-v5';
 const ASSETS = ['./', './index.html', './static/styles.css?v=5', './static/app.js?v=5', './manifest.webmanifest', './static/icon.svg', './static/icon-192.svg', './static/icon-512.svg'];
-self.addEventListener('install', (event) => event.waitUntil(caches.open(CACHE).then((cache) => Promise.all(ASSETS.map((asset) => cache.add(asset).catch(() => null))))));
+self.addEventListener('install', (event) => { self.skipWaiting(); event.waitUntil(caches.open(CACHE).then((cache) => Promise.all(ASSETS.map((asset) => cache.add(asset).catch(() => null))))); });
 self.addEventListener('activate', (event) => event.waitUntil(caches.keys().then((keys) => Promise.all(keys.filter((key) => key !== CACHE).map((key) => caches.delete(key)))).then(() => self.clients.claim())));
 self.addEventListener('fetch', (event) => event.respondWith(caches.match(event.request).then((cached) => cached || fetch(event.request).then((response) => { if (event.request.method === 'GET' && response.ok) { const copy = response.clone(); caches.open(CACHE).then((cache) => cache.put(event.request, copy)); } return response; }).catch(() => caches.match('./index.html')))));
