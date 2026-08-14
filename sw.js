2e4f9d498b6849ac3e00c6e0c40e39d6e6876f3d
joseferreiradev-commits <245466110+joseferreiradev-commits@users.noreply.github.com>
2026-08-14 00:29:12 -0400

    Improve PWA install identity

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const CACHE = 'daily-money-v2';
-const ASSETS = ['./', './index.html', './static/styles.css?v=4', './static/app.js?v=4', './manifest.webmanifest'];
+const CACHE = 'scrap-catcher-v3';
+const ASSETS = ['./', './index.html', './static/styles.css?v=5', './static/app.js?v=5', './manifest.webmanifest', './static/icon.svg'];
 self.addEventListener('install', (event) => event.waitUntil(caches.open(CACHE).then((cache) => Promise.all(ASSETS.map((asset) => cache.add(asset).catch(() => null))))));
 self.addEventListener('activate', (event) => event.waitUntil(caches.keys().then((keys) => Promise.all(keys.filter((key) => key !== CACHE).map((key) => caches.delete(key)))).then(() => self.clients.claim())));
 self.addEventListener('fetch', (event) => event.respondWith(caches.match(event.request).then((cached) => cached || fetch(event.request).then((response) => { if (event.request.method === 'GET' && response.ok) { const copy = response.clone(); caches.open(CACHE).then((cache) => cache.put(event.request, copy)); } return response; }).catch(() => caches.match('./index.html')))));
